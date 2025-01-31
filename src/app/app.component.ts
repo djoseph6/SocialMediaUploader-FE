cc3730ecb44392ce1394cb92bb7936ed745bc7af
Daniel Joseph <danielmjoseph25@gmail.com>
2025-01-30 21:42:12 -0500

    fixed httpclient nullinjection via app.route, app.config and main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { VideoUploadComponent } from './components/video-upload/video-upload.component';
 import { CommonModule } from '@angular/common';
 
+// @NgModule({
+//   declarations: [
+//     VideoUploadComponent
+//   ],
+//   imports: [
+//     CommonModule
+//   ],
+// })
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  providers: [VideoUploadComponent],
+  imports: [CommonModule, VideoUploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
